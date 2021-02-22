46dbb667743f875500e3abad85956ff6a1dc8dda
shraddhavyas9 <78908859+shraddhavyas9@users.noreply.github.com>
2021-02-22 14:55:55 +0530

    Add files via upload

---
+++ get_url/server.js
@@ -0,0 +1,18 @@
+var http = require("http");
+var url = require("url");
+
+function start(){
+    function onRequest(request, response)
+    {
+        var pathname = url.parse(request.url).pathname;
+        console.log("request for" + pathname + "received");
+        response.writeHead(200,{"Content-Type" : "text/plain"});
+        response.write("hello world");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+
+}
+
+exports.start = start;
